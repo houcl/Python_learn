74616103abc7844b9136361c591ab892dea2eab1
侯成龙 <hou755818@163.com>
2016-02-23 20:46:10 +0800

    调用页面

--- flask_restful_test.py
+++ flask_restful_test.py
@@ -15,10 +15,6 @@ parser.add_argument('commodiyt', type=str, help='我也不知道说啥')
 
 datas = {}
 
-class GetWeb(restful.Resource):
-    def get(self):
-        return render_template('commodity.html')
-
 class HelloWorld(restful.Resource):
     def get(self): #实现了get方法
         qwe= "htllo world"
@@ -31,6 +27,7 @@ class GetCommdityID(restful.Resource):
 
         print(commodiyt)
 
+
         self.mysqlDeal = mysql_deal.MysqlDeal()
         #字符串解析
         
@@ -41,10 +38,9 @@ class GetCommdityID(restful.Resource):
 
         #调用数据库链接，添加数据
         r = self.mysqlDeal.insert_(data)
-        rt = {"state":"%s"%r}
 
-        return rt
-    
+        return r
+
     def post(self):
         args = parser.parse_args()
         self.mysqlDeal = mysql_deal.MysqlDeal()
@@ -58,17 +54,19 @@ class GetCommdityID(restful.Resource):
 
         #调用数据库链接，添加数据
         r = self.mysqlDeal.insert_(data)
-        rt = {"state":"%s"%r}
 
-        return rt
+        return r
+
+class GetWeb(restful.Resource):
+    def get(self): #实现了get方法
+        return render_template('templates/commodity.html')
 
 
 api.add_resource(HelloWorld, '/') # 设定路由
 api.add_resource(GetCommdityID, '/commodiyt') # 设定路由
-api.add_resource(GetWeb, '/getweb') # 设定路由
+api.add_resource(GetWeb, '/remove') # 设定路由
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host='123.56.249.33', port=8080)
-
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=8080)
