71a5971e7f7638706cdc2d3ee72bd9adcd161fd9
侯成龙 <hou755818@163.com>
2016-02-23 19:17:25 +0800

    增加

---
+++ mysql_deal.py
@@ -0,0 +1,30 @@
+import pymysql.cursors
+
+connection = pymysql.connect(host='123.56.141.251',
+                             user='root',
+                             password='',
+                             db='dadiao',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+class MysqlDeal(object):
+
+    def insert_(self,data):
+        if data is None:
+            return
+        #数据处理
+        connection.commit()
+
+        print(data)
+
+        with connection.cursor() as cursor:
+            #sql = "INSERT INTO `removesingle` (`CommodityIDList`) VALUES (%s)"
+            sql = "INSERT INTO `dadiao`.`removesuit` (`CommodityIDList`) VALUES (%s);"
+            try:
+                r = cursor.execute(sql,data)
+                return r
+            except Exception as r:
+                return r
+            
+            connection.close()
+            
