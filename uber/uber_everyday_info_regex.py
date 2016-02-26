c39770c386b1a37445114a96fbc048c9166ad83b
侯成龙 <hou755818@163.com>
2016-02-26 20:44:21 +0800

    测试

--- uber/uber_everyday_info_regex.py
+++ uber/uber_everyday_info_regex.py
@@ -7,13 +7,13 @@ datas = {}
 
 class Uber_EveryDay(object):
     def regex_(self,source_html):
-        print(source_html)
         RUGName = "<br/>用户组：(?P<UGName>.+?)（"
         RInTime = "（适用于(?P<InTime>.+?日)）<br/>"
         RArea = "【优步(?P<Area>[^<]+?)奖励政策】"
         RRewardContent = "<img src=\"(?P<RewardContent>[^（]+?)\" style"
         RBest = "获得任何奖励的前提条件：.+?\\*(?P<Best>.+?)<"
-        datas["UGName"] = re.search(RUGName, source_html).group("UGName")
+        
+        datas["UGName"] = re.search(RUGName, source_html).group('UGName')
         datas["InTime"] = re.search(RInTime, source_html).group("InTime")
         datas["UGArea"] = re.search(RArea, source_html).group("Area")
         datas["RewardContent"] = re.search(RRewardContent, source_html).group("RewardContent")
